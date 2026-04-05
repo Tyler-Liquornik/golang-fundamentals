fd72d2318bf52c877746ffe45469fa1afa92cb8f
Tyler Liquornik <tyler.liquornik@gmail.com>
2026-04-05 17:04:02 -0400

    2.1, 2.2, 2.3

---
+++ 2-go-essentials/investment_calculator.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	const inflationRate = 2.5
+	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
+
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+
+	futureValueInflationAdj := futureValue / math.Pow(1+inflationRate/100, years)
+
+	fmt.Println(futureValueInflationAdj)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Tyler-Liquornik/golang-fundamentals
+
+go 1.26.1
