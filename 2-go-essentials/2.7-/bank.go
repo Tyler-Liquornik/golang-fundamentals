3a9e44e6423e1425d0cc8d5923095882a1dccf72
Tyler Liquornik <tyler.liquornik@gmail.com>
2026-04-06 16:41:30 -0400

    control structures

---
+++ 2-go-essentials/2.7-/bank.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+func main() {
+	var accountBalance = 1000.0
+
+	for {
+		fmt.Println("Welcome to Go Bank!")
+		fmt.Println("What do you want to do?")
+		fmt.Println("1. Check balance")
+		fmt.Println("2. Deposit money")
+		fmt.Println("3. Withdraw money")
+		fmt.Println("4. Exit")
+		fmt.Print("Your choice: ")
+
+		var choice int
+		fmt.Print("Your choice: ")
+		fmt.Scan(&choice)
+
+		switch choice {
+		case 1:
+			fmt.Println("Your balance is", accountBalance)
+		case 2:
+			fmt.Print("Your deposit: ")
+			var depositAmount float64
+			fmt.Scan(&depositAmount)
+
+			if depositAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+				continue
+			}
+
+			accountBalance += depositAmount
+			fmt.Println("Your new balance is", accountBalance)
+		case 3:
+			fmt.Print("Your deposit: ")
+			var withdrawAmount float64
+			fmt.Scan(&withdrawAmount)
+
+			if withdrawAmount > accountBalance {
+				fmt.Println("You don't have enough money!")
+				continue
+			} else if withdrawAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+				continue
+			}
+
+			accountBalance -= withdrawAmount
+			fmt.Println("Your new balance is", accountBalance)
+		default:
+			fmt.Println("Goodbye!")
+
+			fmt.Println("Your choice:", choice)
+			fmt.Println("Thank you for using Go Bank!")
+			return
+		}
+	}
+}
